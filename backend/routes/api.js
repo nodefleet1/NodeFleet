2b3bc3dd7d64212a726f004c3df4d8ffa40cf0e7
nodefleet1 <nodefleet1@gmail.com>
2025-10-17 16:36:59 +0530

    Folder Remove because of conflig 17-10-25

--- backend/routes/api.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { chatbotReply } = require("../controller/chatbotController");
-
-// define the POST /api/chat route
-router.post("/chat", chatbotReply);
-
-// export router
-module.exports = router;
